2a85205511b5399c8f613f7245fe745429f0f84e
BrandonMayU <brandonmayu@gmail.com>
2018-04-18 14:57:19 -0700

    Solved 6.13-2

--- 6.13 Problems/6.13-2.py
+++ 6.13 Problems/6.13-2.py
@@ -1,47 +1,27 @@
-#Write a program to draw this. Assume the innermost
-#square is 20 units per side, and each successive
-# square is 20 units bigger, per side, than the one inside it.
-
 import turtle
 
 
-def drawSquare(amountOfSquares):
-
-    sizeOfSquare = 20
-    minusSquare = 5
-
-    for i in range(amountOfSquares):
-        square.penup()
-        square.left(90)
-        square.forward(10)
-        square.right(90)
-        square.back(minusSquare)
-        square.pendown()
-
-
-        for i in range(4):
-            square.forward(sizeOfSquare)
-            square.right(90)
-
-        square.forward(minusSquare)
-
-        newSquare = sizeOfSquare + 20
-        sizeOfSquare = newSquare
-        newMinusSquare = minusSquare + 5
-        minusSquare = newMinusSquare
-        print("Size of Square: ", sizeOfSquare)
-        print("Backwards move: ", minusSquare)
+#This function gets called to draw the square
+def drawSquare():
+    square.penup()
+    square.goto(radius / 2, -radius / 2)
+    square.pendown()
+    square.circle(radius * hypotenuse, steps=4)
 
 
 
+hypotenuse = (2 ** 0.5) / 2
 
+screen = turtle.Screen() #Start Screen
 
-square = turtle.Turtle()
-wn = turtle.Screen()
+squareSize = 20 #Preset the sqaure size for easy changes
 
+square = turtle.Turtle() #Reference the actual turtle to call
 
-drawSquare(4)
+square.setheading(45) #Set the heading so you don't get diamonds
 
+#Use a for loop to repeadtly create squares
+for radius in range(squareSize, squareSize * 5 + 1, squareSize):
+    drawSquare()
 
-
-wn.exitonclick()
+screen.exitonclick()
